0154d4c17cca38220e07303ea00683c37f600cbb
Wooniq <hjw030705@naver.com>
2026-02-27 18:08:18 +0900

    WIP

--- pkg/repository/hana_repo.go
+++ pkg/repository/hana_repo.go
@@ -3,7 +3,8 @@ package repository
 import (
 	"database/sql"
 	"fmt"
-	"strconv" // VIN 변환용 추가
+	"regexp"
+	"strconv" // VIN 변환용
 
 	_ "github.com/SAP/go-hdb/driver"
 )
@@ -36,11 +37,6 @@ func (r *HANARepository) BulkUpsertVehicles(batch []VehicleInfo) error {
 	}
 	defer tx.Rollback()
 
-	// [스키마 일치화] 
-	// 1. "VIN" -> "VEHICLE_ID"로 변경
-	// 2. "UPDATE_STATUS" -> 삭제 (스키마에 없음)
-	// 3. "LAST_REPORTED" -> "LAST_REPORTED_AT"으로 변경
-	// 4. "BATTERY_VOLTAGE" 추가 (기본값 12.0V 설정)
 	query := `UPSERT "Vehicle_ECU_Inventory" (
 		"VEHICLE_ID", 
 		"ECU_TYPE", 
@@ -64,14 +60,14 @@ func (r *HANARepository) BulkUpsertVehicles(batch []VehicleInfo) error {
 		vehicleID := extractIDFromVIN(v.VIN)
 
 		_, err := stmt.Exec(
-			vehicleID,      // 1. VEHICLE_ID (BIGINT)
-			v.ECUType,      // 2. ECU_TYPE
-			v.HWVersion,    // 3. HW_VERSION
-			v.SWMajor,      // 4. SW_MAJOR_V
-			v.SWMinor,      // 5. SW_MINOR_V
-			v.SWPatch,      // 6. SW_PATCH_V
-			v.BatterySOH,   // 7. BATTERY_SOH
-			12.6,           // 8. BATTERY_VOLTAGE (임시 기본값)
+			vehicleID,    // 1. VEHICLE_ID (BIGINT)
+			v.ECUType,    // 2. ECU_TYPE
+			v.HWVersion,  // 3. HW_VERSION
+			v.SWMajor,    // 4. SW_MAJOR_V
+			v.SWMinor,    // 5. SW_MINOR_V
+			v.SWPatch,    // 6. SW_PATCH_V
+			v.BatterySOH, // 7. BATTERY_SOH
+			12.6,         // 8. BATTERY_VOLTAGE (임시 기본값)
 		)
 		if err != nil {
 			return fmt.Errorf("Exec 실패 (ID: %d): %w", vehicleID, err)
@@ -81,14 +77,19 @@ func (r *HANARepository) BulkUpsertVehicles(batch []VehicleInfo) error {
 	return tx.Commit()
 }
 
-// extractIDFromVIN: VIN의 뒷자리 7자리 숫자를 BIGINT ID로 변환 (스키마 호환용)
+// extractIDFromVIN: 문자열에서 숫자만 추출하여 BIGINT로 변환
 func extractIDFromVIN(vin string) int64 {
-	if len(vin) < 7 {
-		return 0
+	// 정규식을 사용하여 문자열 내의 숫자 부분만 추출합니다.
+	re := regexp.MustCompile(`[0-9]+`)
+	matches := re.FindAllString(vin, -1)
+
+	if len(matches) > 0 {
+		// 마지막 숫자 그룹(예: 0001)을 가져와 변환
+		fullDigits := matches[len(matches)-1]
+		id, _ := strconv.ParseInt(fullDigits, 10, 64)
+		return id
 	}
-	// VIN 마지막 7자리가 숫자라고 가정 (시뮬레이션 데이터 기준)
-	id, _ := strconv.ParseInt(vin[len(vin)-7:], 10, 64)
-	return id
+	return 0
 }
 
 func (r *HANARepository) Close() error {
