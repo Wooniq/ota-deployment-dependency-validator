4665b2e3c3eadb11c58a1eeecec540ba987d808e
Wooniq <hjw030705@naver.com>
2026-03-09 03:27:30 +0000

    DB 저장 로직 버그

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,7 @@ import (
 	"os/signal"
 	"syscall"
 	"fmt"
+	"strings"
 
 	"github.com/Wooniq/ota-agent/pkg/repository"
 	"github.com/Wooniq/ota-agent/pkg/service"
@@ -22,7 +23,7 @@ func main() {
 		err = godotenv.Load(".env")
 	}
 
-	// 2. [Repository] 환경 변수를 사용하여 HANA DB 연결
+	// 2. [Repository] HANA DB 연결
 	repo, err := repository.NewHANARepository(
 		os.Getenv("HANA_ADDRESS"),
 		os.Getenv("HANA_PORT"),
@@ -40,25 +41,33 @@ func main() {
 	log.Println("[Step 1] SAP HANA DB 연결 성공")
 
 	// 3. [Transport] Kafka Producer 초기화 (3-Node 클러스터 연결)
-	brokers := []string{"localhost:29092", "localhost:29093", "localhost:29094"}
+	kafkaBrokers := os.Getenv("KAFKA_BROKERS")
+	if kafkaBrokers == "" {
+		kafkaBrokers = "kafka-1:9092,kafka-2:9092,kafka-3:9092"
+	}
+	brokers := strings.Split(kafkaBrokers, ",")
+
+	// Producer 초기화 (수집용)
 	kafkaProducer := transport.NewKafkaProducer(brokers, "ota-inventory")
 	defer kafkaProducer.Close()
 	log.Println("[Step 2] Kafka Producer (3-Node) 준비 완료")
 
 	// 4. [Service] 분석 엔진 초기화
+	// 타겟 버전 설정을 통해 업데이트 대상 차량을 분류합니다.
 	analyzer := service.NewOTAAnalyzer(repo, "v2.2.2", "v1.3.9")
 	log.Println("[Step 3] OTA 분석 엔진 준비 완료")
 
-	// 5. [Transport] Kafka Consumer 기동
-	// Kafka에서 데이터를 실시간으로 꺼내와 분석 엔진(analyzer)에 전달합니다.
-	kafkaConsumer := transport.NewKafkaConsumer(brokers, "ota-inventory", "analyzer-group")
+	// 5. [Transport] Kafka Consumer 기동(분석용)
+	kafkaConsumer := transport.NewKafkaConsumer(brokers, "ota-inventory", "analyzer-group-v2")
 	go kafkaConsumer.StartConsuming(context.Background(), analyzer)
 	log.Println("[Step 4] Kafka Consumer (분석 계층) 가동 중")
 
-	// 6. [Transport] MQTT 수집기 가동 (Kafka Producer 주입)
-	// 이제 MQTT는 분석기에 직접 주지 않고 Kafka로 보냅니다.
-	broker := os.Getenv("MQTT_BROKER")
-	go transport.StartCollector(broker, kafkaProducer) // analyzer 대신 kafkaProducer 전달
+	// 6. [Transport] MQTT 수집기 가동 (입력용)
+	mqttBroker := os.Getenv("MQTT_BROKER")
+	if mqttBroker == "" {
+		mqttBroker = "tcp://mqtt-broker:1883"
+	}
+	go transport.StartCollector(mqttBroker, kafkaProducer) // analyzer 대신 kafkaProducer 전달
 	log.Println("[Step 5] MQTT 수집기 가동 중 (입력 -> Kafka)")
 
 	// 7. Graceful Shutdown
